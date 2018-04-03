192eb932d55c93f8bb0897a8f0674bd2929367b4
Alexander Curtiss <apccurtiss@gmail.com>
2018-04-03 17:31:48 -0600

    Added token-based authentication.

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask, request, redirect, url_for, render_template, make_response
-from datetime import datetime
+from datetime import datetime, timedelta
+from hashlib import md5
+
+import random
+import string
 import sqlite3
 
 app = Flask(__name__)
 tokens = {}
 
+def generate_token(user, size=32):
+    token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+    tokens[token] = (user, datetime.now() + timedelta(hours=24))
+
+    return token
+
 def user_lookup(username, password):
-    query = "SELECT * FROM users WHERE username='%s' AND password='%s'" % (username, password)
+    pass_hash = md5(password.encode('utf-8')).hexdigest()
+    print("Looking up password: %s" % password.encode('utf-8'))
+    print("Hash: %s" % pass_hash)
+    query = "SELECT username FROM users WHERE username='%s' AND password='%s'" % (username, pass_hash)
 
     with sqlite3.connect('data.db') as db:
         user = db.execute(query).fetchone()
 
-    return user
+    return user[0] if user else None
 
 def get_posts(username):
     query = "SELECT * FROM posts WHERE author='%s'" % (username)
@@ -25,6 +38,14 @@ def get_posts(username):
 
     return '\n'.join(map(format_post, posts))
 
+def get_picture(username):
+    query = "SELECT picture FROM users WHERE username='%s'" % (username)
+
+    with sqlite3.connect('data.db') as db:
+        picture = db.execute(query).fetchone()
+
+    return picture[0] if picture else 'default.png'
+
 def create_post(author, posted, content):
     query = "INSERT INTO posts (author, posted, content) VALUES ('%s', '%s', '%s')" % (author, posted, content)
 
@@ -33,22 +54,33 @@ def create_post(author, posted, content):
 
 @app.route("/")
 def index():
-    # print(request.form)
-    # return render_template('index.html')
+    token = request.cookies.get('token', None)
+    if token in tokens:
+        name, exp = tokens[token]
+        if exp > datetime.now():
+            return render_template('index.html', name=name, posts=get_posts(name), picture=get_picture(name))
     return redirect('login')
 
 @app.route("/login", methods = ['GET','POST'])
 def login():
-    if(len(request.form) < 1 or request.method == 'GET'):
+    if(request.method == 'GET'):
         return render_template('login.html')
 
     user = user_lookup(request.form['username'], request.form['password'])
     if user is None:
         return render_template('login.html', invalid=True) # redirect to login if login fails
     else:
-        # resp.set_cookie('token', request.form['username'])
-        name, _ = user
-        return render_template('index.html', name=name, posts=get_posts(name))
+        resp = make_response(redirect('/'))
+        resp.set_cookie('token', generate_token(user))
+
+        return resp
+
+@app.route("/logout", methods = ['GET'])
+def logout():
+    resp = make_response(redirect('login'))
+    resp.set_cookie('token', 'None')
+
+    return resp
 
 @app.route("/post", methods = ['POST'])
 def post():
@@ -56,7 +88,7 @@ def post():
     date_object = datetime.now()
     create_post(name, date_object.strftime('%b %d %I:%M %p'), request.form['content'])
 
-    return render_template('index.html', name=name, posts=get_posts(name))
+    return render_template('index.html', name=name, posts=get_posts(name), picture=get_picture(name))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=1230)
