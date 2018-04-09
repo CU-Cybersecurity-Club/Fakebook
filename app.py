a233a91be7f2e5855b67801610b1dc8583ac4e8e
root <root@marmabot.ml>
2018-04-09 21:45:17 +0000

    Added favicon and moved to port 80.

--- app.py
+++ app.py
@@ -273,4 +273,4 @@ def handle_message(json):
         send({'token': 'invalid'}, json=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0',port=1230)
+    socketio.run(app, host='0.0.0.0',port=80)
