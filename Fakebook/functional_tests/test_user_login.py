dff79fa8913c70ca25cbbd2453a9aec4dbf3ca00
Will Shand <wish5031@colorado.edu>
2019-08-17 13:22:53 -0600

    Fix some of the bugs in the functional tests for user login.

--- Fakebook/functional_tests/test_user_login.py
+++ Fakebook/functional_tests/test_user_login.py
@@ -9,26 +9,26 @@ from .base import FunctionalTest
 class UserLoginTests(FunctionalTest):
     def test_new_user_can_register(self):
         self.browser.get(self.live_server_url)
-        self.assertEqual("Fakebook", self.browser.title)
+        self.assertEqual("Fakebook - Cybersecurity Club", self.browser.title)
 
-        button = self.find_element_by_id("signup-button")
+        button = self.browser.find_element_by_id("signup-button")
         button.click()
 
-        username_input = self.browser.find_element_by_name("username")
-        password_input = self.browser.find_element_by_name("password")
-        repassword_input = self.browser.find_element_by_name("repassword")
+        username_box = self.browser.find_element_by_name("username")
+        password_box = self.browser.find_element_by_name("password")
+        repassword_box = self.browser.find_element_by_name("repassword")
 
-        self.assertEqual(username_input.get_attribute("placeholder"), "Username")
-        self.assertEqual(password_input.get_attribute("placeholder"), "Password")
+        self.assertEqual(username_box.get_attribute("placeholder"), "Username")
+        self.assertEqual(password_box.get_attribute("placeholder"), "Password")
         self.assertEqual(
-            repassword_input.get_attribute("placeholder"), "Re-enter password"
+            repassword_box.get_attribute("placeholder"), "Re-enter password"
         )
         self.assertEqual(password_box.get_attribute("type"), "password")
-        self.assertEqual(repassword_box.gett_attribute("type"), "password")
+        self.assertEqual(repassword_box.get_attribute("type"), "password")
 
-        username_input.send_keys(self.username)
-        password_input.send_keys(self.password)
-        repassword_input.send_keys(self.password)
+        username_box.send_keys(self.username)
+        password_box.send_keys(self.password)
+        repassword_box.send_keys(self.password)
 
         button = self.browser.find_element_by_id("signup-submit-button")
         button.click()
