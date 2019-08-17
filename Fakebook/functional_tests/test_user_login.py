508f56edd73e1fe606dfcfc543105a68eff7d37c
Will Shand <wish5031@colorado.edu>
2019-08-17 13:45:54 -0600

    Add tests to test_user_login.py to ensure that the same user can't register twice. Add some self.fail statements until the code for the existing tests in that file have been completed.

--- Fakebook/functional_tests/test_user_login.py
+++ Fakebook/functional_tests/test_user_login.py
@@ -3,7 +3,7 @@ Tests to determine whether users can sign up, log in, and log out of the
 site correctly.
 """
 
-from .base import FunctionalTest
+from .base import FunctionalTest, generate_random_password
 
 
 class UserLoginTests(FunctionalTest):
@@ -11,8 +11,7 @@ class UserLoginTests(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.assertEqual("Fakebook - Cybersecurity Club", self.browser.title)
 
-        button = self.browser.find_element_by_id("signup-button")
-        button.click()
+        self.browser.find_element_by_id("signup-button").click()
 
         username_box = self.browser.find_element_by_name("username")
         password_box = self.browser.find_element_by_name("password")
@@ -30,5 +29,45 @@ class UserLoginTests(FunctionalTest):
         password_box.send_keys(self.password)
         repassword_box.send_keys(self.password)
 
-        button = self.browser.find_element_by_name("signup-submit-button")
-        button.click()
+        self.browser.find_element_by_name("signup-submit-button").click()
+
+        # TODO: Ensure that Alice arrives at the homepage
+        self.fail("TODO")
+
+    def test_same_user_cannot_register_twice(self):
+        """
+        Once registered, a username should be unavailable
+        """
+
+        def register_user(username, password):
+            self.browser.get(self.live_server_url)
+            self.browser.find_element_by_id("signup-button").click()
+
+            self.browser.find_element_by_name("username").send_keys(self.username)
+            self.browser.find_element_by_name("password").send_keys(self.password)
+            self.browser.find_element_by_name("repassword").send_keys(self.password)
+
+            self.browser.find_element_by_name("signup-submit-button").click()
+
+        # Alice registers herself as a user for the site
+        register_user(self.username, self.password)
+
+        # Now Eve tries to register herself as Alice, using the same username
+        # and password. The site tells her that the username has already been
+        # taken.
+        register_user(self.username, self.password)
+        self.assertTrue(
+            f"Username {self.username} already taken!" in self.browser.page_source
+        )
+
+        # Eve tries again, this time using a different password. The page once
+        # again tells her the username is already taken. In the database, the
+        # entry for the user does not change.
+        new_password = generate_random_password(32)
+        register_user(self.username, new_password)
+        self.assertTrue(
+            f"Username {self.username} already taken!" in self.browser.page_source
+        )
+
+        # TODO: ensure that the database has not changed
+        self.fail("TODO")
