e07e6eb8d3ad0f81125b53685a13d06f0d585915
Will Shand <wish5031@colorado.edu>
2019-08-17 11:58:03 -0600

    Add base class for functional tests.

---
+++ functional_tests/base.py
@@ -0,0 +1,46 @@
+from flask_testing import LiveServerTestCase
+from selenium import webdriver
+from urllib.parse import quote
+import dotenv
+import os
+import random
+import string
+
+"""
+Base class for functional tests
+"""
+
+
+class FunctionalTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        dotenv.load_dotenv()
+        if "STAGING_SERVER" in os.environ:
+            self.live_server_url = "https://" + os.environ["STAGING_SERVER"]
+
+            # Authenticate for staging server
+            username = quote(os.environ["STAGING_SERVER_USERNAME"])
+            password = quote(os.environ["STAGING_SERVER_PASSWORD"])
+            url = "https://%s:%s@%s" % (username, password, staging_server)
+            self.browser.get(url)
+        else:
+            # For local development purposes only
+            self.live_server_url = "http://localhost:8000"
+
+        # Create a test username and password
+        self.email_address = "alice@example.com"
+        self.password = generate_random_password(32)
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+"""
+Helper functions
+"""
+
+
+def generate_random_password(length):
+    characters = string.ascii_uppercase + string.digits
+    random_string = "".join(random.SystemRandom().choices(characters, k=length))
+    return random_string
